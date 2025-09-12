28251af9af3a0d17000a7d4c83295482f25bfc5e
Odette Gallo <odette.gallo@hotmail.com>
2025-09-12 10:59:16 -0300

    veamos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+  <Header />
+ 
+    <Footer />
+    </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="productos.html">TechGadget Store</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="home.html" class="nav-link">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a href="productos.html" class="nav-link active">Productos</a>
+          </li>
+           <li class="nav-item">
+            <a href="inventario.html" class="nav-link active">Inventario</a>
+          </li>
+          <li class="nav-item">
+            <a href="contacto.html" class="nav-link">Contacto</a>
+          </li>
+        </ul>
+        <button id="darkModeToggle" class="btn btn-sm btn-outline-secondary ms-2">
+          🌙
+        </button>
+        <button id="logoutBtn" class="btn btn-sm btn-outline-danger ms-2" style="display: none;">
+          <i class="bi bi-box-arrow-right"></i> Cerrar Sesión
+        </button>
+      </div>
+    </div>
+</nav>
+</template>
+
+
